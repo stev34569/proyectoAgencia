c0eb7c288f155562a6fcc60c63f587aa16bd606e
stev34569 <stevenbans478@gmail.com>
2023-07-27 02:01:42 -0600

    FrondEnt Destinos

---
+++ proyectoAgencia/Controllers/Paquete2Controller.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace proyectoAgencia.Controllers
+{
+    public class Paquete2Controller : Controller
+    {
+        [HttpGet]
+        public IActionResult ConsultarPaquetes2()
+        {
+            return View();
+        }
+    }
+}
